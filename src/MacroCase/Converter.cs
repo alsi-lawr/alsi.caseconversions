158d47583a9fb51e66888c4fcc5355195e57d683
Marcos <m.vinicius.sr11@gmail.com>
2024-09-17 20:35:59 -0300

    MacroCaseConverter Implementation

---
+++ src/MacroCase/Converter.cs
@@ -0,0 +1,18 @@
+﻿namespace ALSI.CaseConversions.MacroCase
+{
+    using System;
+
+    /// <summary>
+    /// For formatting strings to MACRO_CASE.
+    /// </summary>
+    public static class Converter
+    {
+        /// <summary>
+        /// Converts the specified string to MACRO_CASE.
+        /// </summary>
+        /// <param name="stringToConvert">The string to convert.</param>
+        /// <returns>Macro case version of the string.</returns>
+        public static string Convert(ReadOnlySpan<char> stringToConvert) =>
+            Converter<MacroCaseConverter>.ConvertCase(stringToConvert);
+    }
+}
